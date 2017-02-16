c2b11abd55893d5d200932aeef771b4071b2e7fa
Goden Wang <rongguo.wang@qq.com>
2017-02-16 09:52:32 +0800

    RailHelpApp:Menu items show on the bottom.             1. Add ActionMenuItem in WayPicker Activity. 	    2. One item was ignored on the bottom.

--- com/app/src/main/java/railwayhelp/tw/com/RailHelpActivity.java
+++ com/app/src/main/java/railwayhelp/tw/com/RailHelpActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ActionBarOverlayLayout;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
